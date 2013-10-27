ce8f72f6557fc949d342c3a3dd9de890a928873a
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-10-27 18:33:21 +0100

    Zeros

--- test/EnvironmentTest.php
+++ test/EnvironmentTest.php
@@ -4,6 +4,6 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
 {
     public function testAllRunsFineInV8Js()
     {
-        $this->assertSame('', Navigator\pageNumbersText(1, 2, 3, 4));
+        $this->assertSame('', Navigator\pageNumbersText(0, 0, 0, 0));
     }
 }
